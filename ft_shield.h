529f9a5b6e3d087e6ca79053d20bc9357006f034
Song,ch_ <logoshope.song@gmail.com>
2025-04-11 20:45:48 +0200

    added daemonization with systemd in order to be able start service by - creating preconfigurated unit file in systemd service folder - duplicating binary into /bin path to start a service
    visible change: yes

--- ft_shield.h
+++ ft_shield.h
@@ -1,15 +1,29 @@
 #ifndef FT_SHIELD_H
-#define FT_SHIELD_H
+# define FT_SHIELD_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <syslog.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <syslog.h>
+# include <sys/stat.h>
 
-#define BIN_PATH "/bin/ft_shield"
-#define EXE_PATH "/home/s/sh/a.out"
+# define BIN_PATH "/bin/ft_shield"
+# define EXE_PATH "/home/s/sh/a.out"
 
+# define CONFIG "[Unit]\n\
+Description=project ft_shield\n\
+\n\
+[Service]\n\
+Type=simple\n\
+User=root\n\
+WorkingDirectory=/\n\
+ExecStart=/bin/ft_shield\n\
+#Restart=always\n\
+StandardOutput=journal\n\
+\n\
+[Install]\n\
+WantedBy=multi-user.target\n"
 
 #endif

